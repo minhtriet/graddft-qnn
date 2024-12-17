f48bbc0cbb380dc2232fc757934727244201c090
minhtriet <minhtriet09@gmail.com>
2024-12-17 10:50:15 +0700

    draft implement

--- graddft_qnn/dft_qnn.py
+++ graddft_qnn/dft_qnn.py
@@ -1,25 +1,22 @@
 import yaml
+import pennylane as qml
+
 
 class DFTQNN:
     def __init__(self, yaml_file):
         self.circuit = None
-        with open(yaml_file, 'r') as file:
+        with open(yaml_file, "r") as file:
             data = yaml.safe_load(file)
-            if 'num_qubits' not in data:
-                raise KeyError("YAML file must contain 'num_qubits' key")
-            self.num_qubits = data['num_qubits']
-        self.dev = qml.device('default.qubit', wires=self.num_qubits)
+            if "QBIT_PER_AXIS" not in data:
+                raise KeyError("YAML file must contain 'QBIT_PER_AXIS' key")
+            self.num_qubits = data["QBIT_PER_AXIS"] ** 3
 
-
-    @qml.qnode(self.dev)
-    def coefficients(instance, rhoinputs):
+    def circuit(self, theta, phi):
         """
         :param instance: an instance of the class Functional.
         :param rhoinputs: input to the neural network, in the form of an array.
         :return:
         """
-
-        qml.RX(x, wires=0)
-        qml.U1(theta)
-        return qml.state()
-
+        qml.RY(theta, wires=0)
+        qml.U1(phi, wires=0)
+        return qml.probs()
--- graddft_qnn/test.py
+++ graddft_qnn/test.py
@@ -1,16 +1,20 @@
+from optax import apply_updates
 from pyscf import gto, dft
 import grad_dft as gd
-import yaml
 from jax.random import PRNGKey
 from jax import numpy as jnp
+from tqdm import tqdm
+
 from dft_qnn import DFTQNN
-import pennylane as qml
+
+from optax import adam
 
 
 def coefficient_inputs(molecule: gd.Molecule, *_, **__):
     rho = molecule.density()
+    # todo kinetic seems not written in E_{xc}[\rho] = \int \c_\theta[\rho](r) \cdot e[\rho](r)dr
     kinetic = molecule.kinetic_density()
-    return jnp.concatenate((rho, kinetic), axis = 1)
+    return jnp.concatenate((rho, kinetic), axis=1)
 
 
 def energy_densities(molecule: gd.Molecule, clip_cte: float = 1e-30, *_, **__):
@@ -18,32 +22,64 @@ def energy_densities(molecule: gd.Molecule, clip_cte: float = 1e-30, *_, **__):
     # Molecule can compute the density matrix.
     rho = jnp.clip(molecule.density(), a_min=clip_cte)
     # Now we can implement the LDA energy density equation in the paper.
-    lda_e = -3/2 * (3/(4*jnp.pi)) ** (1/3) * (rho**(4/3)).sum(axis = 1, keepdims = True)
+    lda_e = (
+        -3
+        / 2
+        * (3 / (4 * jnp.pi)) ** (1 / 3)
+        * (rho ** (4 / 3)).sum(axis=1, keepdims=True)
+    )
     # For simplicity we do not include the exchange polarization correction
     # check function exchange_polarization_correction in functional.py
     # The output of features must be an Array of dimension n_grid x n_features.
     return lda_e
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dft_qnn = DFTQNN("config.yaml")
 
-    # Define the geometry of the molecule and mean-field object
     mol = gto.M(atom=[["H", (0, 0, 0)]], basis="def2-tzvp", charge=0, spin=1)
-    mf = dft.UKS(mol)
-    mf.kernel()
+    mean_field = dft.UKS(mol)
+    ground_truth_energy = mean_field.kernel()
+
     # Then we can use the following function to generate the molecule object
-    HF_molecule = gd.molecule_from_pyscf(mf)
+    HF_molecule = gd.molecule_from_pyscf(mean_field)
     coefficients = dft_qnn.circuit()
 
     nf = gd.Functional(coefficients, energy_densities, coefficient_inputs)
+    key = PRNGKey(42)
+    cinputs = coefficient_inputs(HF_molecule)
 
+    # Init the params
+    params = nf.init(key, cinputs)
     # Start the training
 
+    learning_rate = 0.01
+    momentum = 0.9
+    tx = adam(learning_rate=learning_rate, b1=momentum)
+    opt_state = tx.init(params)
     key = PRNGKey(42)
     cinputs = coefficient_inputs(HF_molecule)
     params = nf.init(key, cinputs)
 
-    params = nf.init(key, cinputs)
-
     E = nf.energy(params, HF_molecule)
+
+    # todo predictor is for simple energy loss func but why
+    predictor = gd.non_scf_predictor(nf)
+
+    # training loop
+    # todo n_epochs from yaml instead
+    n_epochs = 3
+    for iteration in tqdm(range(n_epochs), desc="Training epoch"):
+        (cost_value, predicted_energy), grads = gd.simple_energy_loss(
+            params, predictor, HF_molecule, ground_truth_energy
+        )
+        print(
+            "Iteration",
+            iteration,
+            "Predicted energy:",
+            predicted_energy,
+            "Cost value:",
+            cost_value,
+        )
+        updates, opt_state = tx.update(grads, opt_state, params)
+        params = apply_updates(params, updates)
