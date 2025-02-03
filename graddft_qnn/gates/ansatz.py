74f34e76afc05762e6a702d385a11a78cc64f964
minhtriet <minhtriet09@gmail.com>
2025-02-03 22:29:44 +0000

    add zzz gate

--- graddft_qnn/gates/ansatz.py
+++ graddft_qnn/gates/ansatz.py
@@ -5,23 +5,29 @@ import pennylane.numpy as np
 
 
 class Ansatz(qml.operation.Operation):
+    """
+    XYZ
+    XYZ --- ZZZ
+    XYZ
+    equation 55 JJMeyer
+    """
     num_wires = 3
     num_params = 4
 
     def __init__(self, theta_x, theta_y, theta_z, theta_entanglement, wires):
+        """
+        thetas are placeholder in case we want them to be learnable params
+        """
         super().__init__(theta_x, theta_y, theta_z, theta_entanglement, wires=wires)
         self._parameters = [theta_x, theta_y, theta_z, theta_entanglement]
 
     @staticmethod
     def compute_matrix(*params, **hyperparams: dict[str, Any]):
-        """
-        equation 55 JJMeyer
-        """
         per_wire = (
             qml.X.compute_matrix() @ qml.Y.compute_matrix() @ qml.Z.compute_matrix()
         )
         all_wires = np.kron(np.kron(per_wire, per_wire), per_wire)
-        all_wires = all_wires @ Ansatz._RXXX_matrix(0.1)
+        all_wires = all_wires @ Ansatz._ZZZ_matrix()
 
         return all_wires
 
@@ -40,4 +46,16 @@ class Ansatz(qml.operation.Operation):
                 [0, sin, 0, 0, 0, 0, cos, 0],
                 [sin, 0, 0, 0, 0, 0, 0, cos],
             ]
+        )
+
+    def _ZZZ_matrix(self):
+        return np.array([
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, -1, 0, 0, 0, 0, 0, 0],
+            [0, 0, -1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, -1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, -1]]
         )
