8d50434164920b56fced30214ee18997f9f7826c
minhtriet <minhtriet09@gmail.com>
2025-04-22 20:59:56 +0100

    add test case for invariant 6 qb d4

--- tests/test_circuit_equivariance.py
+++ tests/test_circuit_equivariance.py
@@ -22,12 +22,6 @@ def test_a_training_step():
     rot_mock_coeff_inputs_x = (
         O_h._180_deg_x_rot(int(np.cbrt(2**num_wires))) @ mock_coeff_inputs
     )
-    # rot_mock_coeff_inputs_y = (
-    #     O_h._180_deg_y_rot(int(np.cbrt(2**num_wires))) @ mock_coeff_inputs
-    # )
-    # rot_mock_coeff_inputs_z = (
-    #     O_h._180_deg_z_rot(int(np.cbrt(2**num_wires))) @ mock_coeff_inputs
-    # )
 
     key = PRNGKey(42)
 
@@ -35,10 +29,46 @@ def test_a_training_step():
 
     result = dft_qnn.apply(parameters, mock_coeff_inputs)
     result_rot_x = dft_qnn.apply(parameters, rot_mock_coeff_inputs_x)
-    # result_rot_y = dft_qnn.apply(parameters, rot_mock_coeff_inputs_y)
-    # result_rot_z = dft_qnn.apply(parameters, rot_mock_coeff_inputs_z)
-
     assert np.allclose(result_rot_x, result)
-    # assert np.allclose(result_rot_y, result, atol=1e-6)
-    # assert np.allclose(result_rot_z, result, atol=1e-6)
-    # assert np.allclose(result_rot_z_rot_x, result, atol=1e-6)
+
+
+def test_a_training_step_6qb_d4():
+    num_wires = 6
+    _setup_device = qml.device("default.qubit", num_wires)
+    gates_indices = list(np.random.choice(2**num_wires, num_wires, replace=False))
+    filename = pathlib.Path("tests") / "ansatz_6_d4.pkl"
+    gates_gen = AnsatzIO.read_from_file(str(filename))
+    measurement_expvals = gates_gen
+    mock_coeff_inputs = np.random.rand(2**num_wires)
+    dft_qnn = DFTQNN(_setup_device, gates_gen, measurement_expvals, gates_indices)
+
+    rot_mock_coeff_inputs_x = (
+        O_h._180_deg_x_rot(int(np.cbrt(2**num_wires))) @ mock_coeff_inputs
+    )
+    rot_mock_coeff_inputs_y = (
+        O_h._180_deg_y_rot(int(np.cbrt(2**num_wires))) @ mock_coeff_inputs
+    )
+    rot_mock_coeff_inputs_z = (
+        O_h._180_deg_z_rot(int(np.cbrt(2**num_wires))) @ mock_coeff_inputs
+    )
+    rot_mock_coeff_inputs_x_y_eq_z = (
+        O_h._270_deg_x_rot(int(np.cbrt(2**num_wires)))
+        @ O_h.y_eq_z_rot(int(np.cbrt(2**num_wires)))
+        @ mock_coeff_inputs
+    )
+    key = PRNGKey(42)
+
+    parameters = dft_qnn.init(key, mock_coeff_inputs)
+
+    result = dft_qnn.apply(parameters, mock_coeff_inputs)
+    result_rot_x = dft_qnn.apply(parameters, rot_mock_coeff_inputs_x)
+    result_rot_y = dft_qnn.apply(parameters, rot_mock_coeff_inputs_y)
+    result_rot_z = dft_qnn.apply(parameters, rot_mock_coeff_inputs_z)
+    result_rot_mock_coeff_inputs_x_y_eq_z = dft_qnn.apply(
+        parameters, rot_mock_coeff_inputs_x_y_eq_z
+    )
+
+    assert np.allclose(result_rot_x, result, 2e-5)
+    assert np.allclose(result_rot_y, result, 2e-5)
+    assert np.allclose(result_rot_z, result, 2e-5)
+    assert np.allclose(result_rot_mock_coeff_inputs_x_y_eq_z, result, 2e-5)
