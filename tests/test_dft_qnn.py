70a61e3d49bfabb0025951f577b2964abc700c2c
minhtriet <minhtriet09@gmail.com>
2025-05-14 23:17:15 +0100

    fix append multiple time

--- tests/test_dft_qnn.py
+++ tests/test_dft_qnn.py
@@ -8,22 +8,17 @@ from graddft_qnn.unitary_rep import O_h
 
 def test_twirling():
     # eq.60 in https://doi.org/10.1103/PRXQuantum.4.010328
-    twirled = DFTQNN._twirling(qml.X(0) @ qml.I(1), [qml.SWAP([0, 1])])
+    twirled = DFTQNN._twirling(
+        qml.X(0) @ qml.I(1), [qml.SWAP([0, 1]), qml.I(0) @ qml.I(1)]
+    )
     expected = 0.5 * (qml.X(0) + qml.X(1))
     assert np.allclose(qml.matrix(twirled), qml.matrix(expected))
 
     # eq.65 in https://doi.org/10.1103/PRXQuantum.4.010328
-    twirled = DFTQNN._twirling(qml.I(0) @ qml.Y(1), [qml.X(0) @ qml.X(1)])
-    assert twirled is None
-
-    rep = qml.I(0) @ qml.X(1) @ qml.X(2)
-    ansatz = qml.X(0) @ qml.Y(1) @ qml.Z(2)
-    twirled = DFTQNN._twirling(ansatz, [rep])
-    expected = 0.5 * (
-        rep @ ansatz @ qml.adjoint(rep)
-        + (qml.I(0) @ qml.I(1)) @ ansatz @ qml.adjoint(qml.I(0) @ qml.I(1)) @ qml.I(2)
+    twirled = DFTQNN._twirling(
+        qml.I(0) @ qml.Y(1), [qml.X(0) @ qml.X(1), qml.I(0) @ qml.I(1)]
     )
-    assert np.allclose(qml.matrix(twirled), qml.matrix(expected))
+    assert twirled is None
 
 
 @pytest.mark.skip(reason="takes too long, can run separately")
